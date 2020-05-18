a687b80594158267b1580daa7bd73e927902954f
MatinMirzaei <45024401+MatinMirzaei@users.noreply.github.com>
2020-05-18 12:26:01 -0700

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ScaffoldIdentiti.Data;
+
+[assembly: HostingStartup(typeof(ScaffoldIdentiti.Areas.Identity.IdentityHostingStartup))]
+namespace ScaffoldIdentiti.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ScaffoldIdentitiContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ScaffoldIdentitiContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ScaffoldIdentitiContext>();
+            });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ScaffoldIdentiti.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
